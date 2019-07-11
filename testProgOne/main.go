3dd96a6f03f5eaedb43847d0ab6f30d04a34bb85
J.Hawkes <jh923@kent.ac.uk>
2019-07-11 20:25:57 +0100

    done test 2

--- testProgOne/main.go
+++ testProgOne/main.go
@@ -13,6 +13,7 @@ func main() {
 	//convert()
 	//test1()
 	//con()
+	test2()
 }
 
 /**
@@ -130,4 +131,46 @@ func con() {
 	fmt.Printf("%b\n", kb)
 	fmt.Printf("%b\n", mb)
 	fmt.Printf("%b\n", gb)
+}
+
+
+func test2() {
+	//test 2.1 - print a number in base 10, base 2 and base 16
+	num := 100
+	fmt.Printf("%d\t%b\t%X\n", num, num, num)
+
+	//test 2.2 - comparing values
+	a := 10
+	b := 100
+	g := a == b
+	h := a <= b
+	j := a != b
+	k := a <  b
+	l := a >  b
+	fmt.Println(g,h,j,k,l)
+
+	//test 2.3 - constants
+	const t23 = 23
+	const t231 int = 23
+	fmt.Printf("%v\t%v\n", t23, t231)
+
+	//test 2.4 - bit shift
+	a = 7
+	fmt.Printf("%d\t%b\t%X\n", a, a, a)
+	a = a << 1
+	fmt.Printf("%d\t%b\t%X\n", a, a, a)
+
+	//test 2.5 - raw string literal
+	c := `Literal string`
+	fmt.Println(c)
+
+	//test 2.6 - iota
+	const  (
+		_  = 2019 + iota
+		n1 = 2019 + iota
+		n2 = 2019 + iota
+		n3 = 2019 + iota
+		n4 = 2019 + iota
+	)
+	fmt.Println(n1,n2,n3,n4)
 }
