24f43be9fdcf2892f9aae142be1d093573542b0e
J.Hawkes <jh923@kent.ac.uk>
2019-07-11 13:47:23 +0100

    added note on vfariable scope

--- testProgOne/main.go
+++ testProgOne/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+var a string = "global scoped var"	//var name type. Type is not needed as the compiler can figure it out. But locks the scope of the variable
+
 func main()  {
-	fmt.Print("Yeet")
+	fmt.Println("printing")
+
+	x := 10	//Can't be outside of a function body
+	fmt.Println(x*x)
+
+	var y = "within the scope of function main" //can be declared outside of function body.
+	fmt.Println(y)
+}
+
+func foo()	{
+	fmt.Println(a)
 }
