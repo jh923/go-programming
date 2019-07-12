a75a023d3026eeb086ccc5737d7d298f3d28d8de
J.Hawkes <jh923@kent.ac.uk>
2019-07-12 19:24:09 +0100

    Finished third test

--- testProgOne/main.go
+++ testProgOne/main.go
@@ -13,7 +13,10 @@ func main() {
 	//convert()
 	//test1()
 	//con()
-	test2()
+	//test2()
+	//switchExample()
+	//test3()
+	groupData()
 }
 
 /**
@@ -46,7 +49,7 @@ func convert() {
 	num = 100
 	fmt.Printf("%T\n", num) //num is of type sInt not int and thus cannot be converted to an int
 	a := 42
-	a = int(num)	//int(value) converts a value to an int, in the from T(x) where T is the desired type and x is a value which can be converted to type T
+	a = int(num) //int(value) converts a value to an int, in the from T(x) where T is the desired type and x is a value which can be converted to type T
 	fmt.Println(a)
 
 }
@@ -54,10 +57,11 @@ func convert() {
 var x int = 42
 var y string = "James Bond"
 var z bool = true
+
 /**
 The tests can be found at https://docs.google.com/document/d/1ckYpi6hcRkaBUEk975f54oGsHYHu7GhzOk7-nOrkNxo/edit#
- */
-func test1()	{
+*/
+func test1() {
 	fmt.Println("Test 1.1 - 1.3")
 	//x := 42
 	//y := "James Bond"
@@ -79,7 +83,7 @@ func test1()	{
 	fmt.Println("\nTest 1.5")
 
 	var y int
-	fmt.Printf("%v\n", x)	//is 42 from prev exercise
+	fmt.Printf("%v\n", x) //is 42 from prev exercise
 	fmt.Printf("%T\n", x)
 	x = 24
 	fmt.Printf("%v\n", x)
@@ -87,18 +91,17 @@ func test1()	{
 	fmt.Printf("%v\n", y)
 	fmt.Printf("%T\n", y)
 
-
 }
 
 /*
 defining constants
 Constants have their expected type and value are are immutable
- */
+*/
 func con() {
 	//Different ways of defining values as constants
 	const (
-		jeff= "my name is"
-		notTrue= false
+		jeff    = "my name is"
+		notTrue = false
 	)
 
 	const scrub bool = true
@@ -112,7 +115,7 @@ func con() {
 	fmt.Println(scrub)
 
 	//iota - used to count up in constants
-	const  (
+	const (
 		a0 = iota
 		a1
 		a2
@@ -122,7 +125,7 @@ func con() {
 	//using iota in bit shift to denote memory
 
 	const (
-		_ = iota	// _ discard char
+		_  = iota             // _ discard char
 		kb = 1 << (iota * 10) // 1 shift left 10
 		mb = 1 << (iota * 10) // 1 shift left 20
 		gb = 1 << (iota * 10) // 1 shift left 30
@@ -133,7 +136,9 @@ func con() {
 	fmt.Printf("%b\n", gb)
 }
 
-
+/**
+The tests can be found at https://docs.google.com/document/d/1ckYpi6hcRkaBUEk975f54oGsHYHu7GhzOk7-nOrkNxo/edit#
+*/
 func test2() {
 	//test 2.1 - print a number in base 10, base 2 and base 16
 	num := 100
@@ -145,9 +150,9 @@ func test2() {
 	g := a == b
 	h := a <= b
 	j := a != b
-	k := a <  b
-	l := a >  b
-	fmt.Println(g,h,j,k,l)
+	k := a < b
+	l := a > b
+	fmt.Println(g, h, j, k, l)
 
 	//test 2.3 - constants
 	const t23 = 23
@@ -165,12 +170,113 @@ func test2() {
 	fmt.Println(c)
 
 	//test 2.6 - iota
-	const  (
+	const (
 		_  = 2019 + iota
 		n1 = 2019 + iota
 		n2 = 2019 + iota
 		n3 = 2019 + iota
 		n4 = 2019 + iota
 	)
-	fmt.Println(n1,n2,n3,n4)
+	fmt.Println(n1, n2, n3, n4)
+}
+
+func switchExample() {
+	//Switch by default has no fall though. Meaning if multiple statements are true only the first true one will be executed.
+	switch {
+	case (2%2 == 1):
+		fmt.Println("This will not print ever")
+	case (2 == 2):
+		fmt.Println("this will print")
+	case (true == true):
+		fmt.Println("although this is true this statement is unreachable.")
+	}
+
+	//reason toi not use fall though
+	switch {
+	case (2%2 == 1):
+		fmt.Println("This will not print ever")
+		fallthrough
+	case (2 == 2):
+		fmt.Println("this will print")
+		fallthrough
+	case (true == true):
+		fmt.Println("This statement is reached due to fall thought")
+		fallthrough
+	case (false == true):
+		fmt.Println("Due to fallthrough this statement is printed although it is false")
+	default:
+		fmt.Println("Default")
+	}
+
+}
+
+func test3() {
+	//print 1<=x<=10
+	for i := 1; i < +10; i++ {
+		fmt.Println(i)
+	}
+
+	fmt.Println()
+	fmt.Println()
+
+	//print utf-8 capital english letters 3 times each and their respective number
+	for i := 65; i < 91; i++ {
+		fmt.Println(i)
+		for j := 0; j < 3; j++ {
+			fmt.Printf("\t%#U\n", i)
+		}
+	}
+
+	//loop until age is correctly guessed
+
+	age := 21
+	guess := 0
+	for {
+		if guess == age {
+			fmt.Printf("Your ages is: %d", guess)
+			break
+		}
+		guess++
+	}
+
+	//find x mod 4 where 10<=x<=25
+	for i := 10; i <= 25; i++ {
+		fmt.Println(i % 4)
+	}
+
+	fmt.Println()
+	fmt.Println()
+
+	if age < 18 {
+		fmt.Println("You can't drink in the UK or US")
+	} else if age >= 18 && age < 21 {
+		fmt.Println("You can drink in the UK but not the US")
+	} else {
+		fmt.Println("You can drink in the UK and the US")
+	}
+
+	fmt.Println()
+	fmt.Println()
+
+	favSport := "Golf"
+
+	switch favSport {
+	case "DMM", "LCS", "Basketball", "Rugby":
+		fmt.Printf("I like %v too!", favSport)
+
+	case "Football", "Golf", "Baseball":
+		fmt.Printf("I don't like %v", favSport)
+	default:
+		fmt.Println("I don't know that sport")
+	}
+
+	fmt.Println()
+	fmt.Println()
+
+	//true, false, true, false, false
+
+}
+
+func groupData()	{
+
 }
