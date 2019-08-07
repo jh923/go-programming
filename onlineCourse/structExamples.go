449fe22bbb46a225d661febdbe599fb58f8a4f89
J.Hawkes <jh923@kent.ac.uk>
2019-08-07 15:36:22 +0100

    Updated struct exampole to include anons

--- onlineCourse/structExamples.go
+++ onlineCourse/structExamples.go
@@ -7,14 +7,23 @@ func main()  {
 }
 
 func structExamples() {
+	// A defined struct
 	type person struct {
 		first string
 		last string
 	}
+	// A struct with inheritance
+	type programmer struct {
+		person
+		langs []string
+	}
 
-	p1 := person{
-		first: "Callum",
-		last: "Russel",
+	p1 := programmer{
+		person : person{
+			first: "Callum",
+			last:  "Russel",
+		},
+		langs: []string{"Java", "PHP", "HTML", "CSS"},
 	}
 
 	p2 := person{
@@ -23,9 +32,24 @@ func structExamples() {
 	}
 
 	fmt.Println(p1)
-	fmt.Println(p1.first, p1.last)
+	fmt.Println(p1.first, p1.last, "knows: " , p1.langs)
 	fmt.Println(p2)
 	fmt.Println(p2.first, p2.last)
+
+	//A anon struct
+	p3 := struct {
+		first string
+		last string
+		age int
+		married bool
+	}{
+		first: "Aivis",
+		last: "Jak",
+		age: 21,
+		married: false,
+	}
+
+	fmt.Println(p3)
 
 
 }
---
+++ onlineCourse/testFive.go
@@ -0,0 +1,74 @@
+/**
+The tests can be found at https://docs.google.com/document/d/1ckYpi6hcRkaBUEk975f54oGsHYHu7GhzOk7-nOrkNxo/edit#
+*/
+
+package main
+
+import "fmt"
+
+func main()	{
+	test5()
+}
+
+func test5() {
+	type person struct {
+		first string
+		last string
+		favFlavor []string
+	}
+
+	p1 := person{
+		first: "Jeff",
+		last: "Jones",
+		favFlavor: []string{
+			"Choc",
+			"Strawberry",
+			"Plain"},
+	}
+
+	p2 := person{
+		first: "Nadir",
+		last: "Farfa",
+		favFlavor: []string{
+			"Mint",
+			"Lemon",
+			"Strawberry"},
+	}
+
+	//Done using for loop
+	fmt.Printf("%s %s likes the following types of ice creams: ", p1.first, p1.last)
+	for i := 0; i < len(p1.favFlavor); i++ {
+		fmt.Print(p1.favFlavor[i], " ")
+	}
+
+	fmt.Printf("\n%s %s likes the following types of ice creams: ", p2.first, p2.last)
+	for i := 0; i < len(p2.favFlavor); i++ {
+		fmt.Print(p2.favFlavor[i], " ")
+	}
+
+	//Done using range
+	fmt.Printf("\n%s %s likes the following types of ice creams: ", p1.first, p1.last)
+	for _, v := range p1.favFlavor{
+		fmt.Print(v, " ")
+	}
+
+	fmt.Printf("\n%s %s likes the following types of ice creams: ", p2.first, p2.last)
+	for _, v := range p2.favFlavor{
+		fmt.Print(v, " ")
+	}
+
+	fmt.Println()
+
+	m1 := map[string]person{
+		p1.last : p1,
+		p2.last: p2,
+	}
+
+	for k, v := range m1 {
+		fmt.Println("\n", k)
+		for i, v2 := range v.favFlavor  {
+			fmt.Println(i, v2)
+		}
+	}
+
+}
