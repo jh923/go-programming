f0fc090b2067b5cbe6c1bee052e7316d38bb4c73
J.Hawkes <jh923@kent.ac.uk>
2019-08-07 20:48:14 +0100

    using an function as an argument

--- onlineCourse/funcExpression.go
+++ onlineCourse/funcExpression.go
@@ -12,6 +12,11 @@ func main() 	{
 	fmt.Printf("%T\n", gorwell())
 	fmt.Println(gorwell())
 	fmt.Println(bar()())
+
+
+	ls := []int{1,2,3,4,5,6,7,8,9}
+	mul := even(mul, ls...)
+	print(mul)
 }
 
 func orwell() func() int{
@@ -24,4 +29,22 @@ func bar() func() int {
 	return func() int {
 		return 451
 	}
+}
+
+func mul(is ...int) int {
+	total := 1
+	for _, v := range is	{
+		total = total * v
+	}
+	return total
+}
+
+func even(f func(is ...int) int, xs ...int)	int {
+	var ys []int
+	for _, v := range xs	{
+		if v%2 == 0 {
+			ys = append(ys, v)
+		}
+	}
+	return f(ys...)
 }
