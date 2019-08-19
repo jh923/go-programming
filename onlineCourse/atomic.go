71bc902e4be35a1bba380a60036fc75933c17f23
J.Hawkes <jh923@kent.ac.uk>
2019-08-19 14:43:59 +0100

    Finished section 20

---
+++ onlineCourse/atomic.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"sync/atomic"
+)
+
+func main()	{
+	var counter int64
+	const gs = 100
+	var wg = sync.WaitGroup{}
+	wg.Add(gs)
+
+	for i := 0; i < gs; i++	{
+		go func() {
+			atomic.AddInt64(&counter, 1)
+			runtime.Gosched()
+			fmt.Println("Counter\t", atomic.LoadInt64(&counter))
+			wg.Done()
+		}()
+		fmt.Println("Goroutines:\t", runtime.NumGoroutine())
+	}
+	wg.Wait()
+}
--- onlineCourse/waitGroup.go
+++ onlineCourse/waitGroup.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
 )
 
 var wg sync.WaitGroup
+var mu sync.Mutex
 
 func main()	{
 	go count()
@@ -19,6 +21,24 @@ func main()	{
 		ch <- doSomething(5)
 	}()
 	fmt.Println(<-ch)
+
+	counter := 0
+	const gs = 100
+	wg.Add(gs)
+	for i := 0; i < gs; i++ {
+		go func() {
+			//Locks when entering a critical section of a shared variable and unlocks when exiting it
+			mu.Lock()
+			v := counter
+			runtime.Gosched()
+			v++
+			counter = v
+			mu.Unlock()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println("Counter value:", counter)
 }
 
 func doSomething(x int) int {
