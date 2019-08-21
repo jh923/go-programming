f81ce812885f4f4dd293eeb4ceb7dc82b2c90dd3
J.Hawkes <jh923@kent.ac.uk>
2019-08-21 13:30:56 +0100

    table test example

---
+++ onlineCourse/notes/12-Testing/tableTestPowerExample/power_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func TestPower(t *testing.T) {
+
+	type test struct {
+		data []int
+		answer int
+	}
+
+	tests := []test{
+		{[]int{1,3}, 1},
+		{[]int{2,3}, 8},
+		{[]int{3,3}, 27},
+	}
+
+	for _, v := range tests	{
+		x := power(v.data[0], v.data[1])
+		if x != v.answer	{
+			t.Error("Expected", v.answer, "got", x)
+		}
+	}
+}
